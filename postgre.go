b312998e4a7246137ed45acb826c58c19273710a
ArtemkaKun <zumikq78@gmail.com>
2020-02-24 09:36:47 +0100

    DBs init connect

--- postgre.go
+++ postgre.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4"
+	"os"
+)
+
+func init() {
+	_, err := pgx.Connect(context.Background(), "postgres://tester:alpha-omega@localhost:5432/test_task")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connection to database: %v\n", err)
+	} else {
+		fmt.Println("Connected to PSQL!")
+	}
+}
